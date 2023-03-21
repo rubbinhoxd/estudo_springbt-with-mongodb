e26f00d1477301e9bd4062477538d859e762c07c
rubbinhoxd <fracicorubens@yahoo.com>
2023-03-20 21:17:46 -0300

    Project created

---
+++ src/main/java/com/example/estudoSpringWithMongo/EstudoSpringWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.estudoSpringWithMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringWithMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estudoSpringWithMongo/EstudoSpringWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudoSpringWithMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringWithMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
